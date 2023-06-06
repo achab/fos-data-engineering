862285d67c510dba9b1bed8b153ac0b57487e13e
Massil Achab <massil.achab@gmail.com>
2023-06-06 12:02:15 +0200

    added doc to transform functions

--- transform.py
+++ transform.py
@@ -5,15 +5,26 @@ import pandas as pd
 
 
 def get_all_filenames():
+    # this function should return a list of filenames
+    # you should look at glob library
+    # Example: it should return ["raw_data_1686027160.json", "raw_data_1686027165.json", "raw_data_1686027170.json"]
     pass
 
 def transform_one_file(filename):
+    # this function should return a list of dictionaries
+    # each dictionnary should look like that: { "city": city, "latitude": latitude, "longitude": longitude , "temperature": tempature, "time": time}
     pass
 
 def merge_all_files_in_pandas_df(files):
-    pass
+    output = []
+    for fname in files:
+        output_one_file = transform_one_file(fname)
+        output.extend(output_one_file)
+    df = pd.DataFrame(output)
+    return df
 
 def drop_duplicates(df_):
+    # drop duplicated rows using a function of pandas.DataFrame
     pass
 
 def main():
