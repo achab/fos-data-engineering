f624abe2cbad8c502972275c65bbcd674aed0258
Massil Achab <massil.achab@gmail.com>
2023-06-06 11:57:13 +0200

    added solution for extract

--- extract.py
+++ extract.py
@@ -7,18 +7,26 @@ import requests
 def get_list_of_cities():
     # read the file cities.json and returns its content.
     # Example: this function should return ["Algiers", "Batna", "Tamanrasset"]
-    pass
+    with open("cities.json", "r") as f:
+        cities = json.load(f)
+    return cities
 
 def get_lat_lon(city):
     # get latitude and longitude data of cities in Algeria 
     # using the API documented here: https://nominatim.org/release-docs/latest/api/Search/
     # Example: this function should return 36.6875, 3.125
-    pass
+    url = f"https://nominatim.openstreetmap.org/search?city={city}&country=Algeria&format=json"
+    res = requests.get(url).json()
+    lat = res[0]["lat"]
+    lon = res[0]["lon"]
+    return lat, lon
 
 def get_current_weather(lat, lon):
     # get current weather data at (latitude, longitude)
     # using the API documented here: https://open-meteo.com/
-    pass
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    resp = requests.get(url).json()
+    return resp
 
 def get_weather_all_cities(cities):
     data = dict()
